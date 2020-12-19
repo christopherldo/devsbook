ba20cf01c179bb9ab8ffc58ed0f137a7bed130bb
Christopher de Oliveira <christopherforwork@gmail.com>
2020-12-19 10:23:33 -0400

    Structured Databse and structured the base

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$base = 'http://localhost/';
+
+$db_name = 'devsbook';
+$db_host = 'localhost';
+$db_user = 'dev';
+$db_pass = 'dev';
+
+$pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_pass);
