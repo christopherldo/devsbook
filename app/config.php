6c163b9b35df1d119ce866bec37cded36e214f6d
Christopher de Oliveira <christopherldo@gmail.com>
2024-09-07 12:15:45 -0400

    Adds more envinroment variables

--- app/config.php
+++ app/config.php
@@ -2,11 +2,12 @@
 
 session_start();
 
-$base = getenv("BASE_URL");
+$base = getenv("BASE_URL") . ":" . getenv("PORT");
 
-$db_name = getenv("DATABASE_USER");
-$db_host = 'db';
+$db_name = getenv("DATABASE_NAME");
+$db_host = getenv("DATABASE_HOST");
+$db_port = getenv("DATABASE_PORT");
 $db_user = getenv("DATABASE_USER");
 $db_pass = getenv("DATABASE_PASSWORD");
 
-$pdo = new PDO("pgsql:dbname=$db_name;host=$db_host", $db_user, $db_pass);
+$pdo = new PDO("pgsql:dbname=$db_name;host=$db_host;port=$db_port", $db_user, $db_pass);
